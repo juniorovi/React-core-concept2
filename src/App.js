f47f48d663afd3971f08272553f4aff71c6e24c1
juniorovi <mdtarikulislam258000@gmail.com>
2023-01-02 18:59:24 +0600

    Pass value by identifier and catch by props and clear array of object catching

--- src/App.js
+++ src/App.js
@@ -2,24 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const properties = [
+    {name: 'Land', price: '2lak'},
+    {name: 'Business', price: '1.5lak'}
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Product name="Laptop" price="15000Tk"></Product>
+      <Product name="Bike" price="126000Tk"></Product>
+
+      {
+        properties.map(property => <Property name={property.name} price={property.price}></Property>)
+      }
+      
     </div>
   );
+}
+
+function Product (props){
+  return(
+    <div className='product'>
+      <h1>Product</h1>
+      <h3>Name: {props.name}</h3>
+      <p>Price: {props.price}</p>
+    </div>
+  );
+}
+
+function Property(props){
+  return(
+    <div className="property">
+      <h1>Property</h1>
+      <h3>Name: {props.name}</h3>
+      <p>Price: {props.price}</p>
+
+    </div>
+  )
 }
 
 export default App;
